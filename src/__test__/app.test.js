b9060811fba393dff5aa62f30eccb6cc78cb107d
rka <russkikh_k@ekb.sima-land.ru>
2021-11-02 22:48:39 +0500

    done

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -15,3 +15,19 @@ test('sort test', () => {
   SortByHealthLevel(arrTest);
   expect(arrTest).toEqual(arrRight);
 });
+
+test('sort test not tobe', () => {
+    const arrTest = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ];
+    const arrRight = [
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+    ];
+
+    SortByHealthLevel(arrTest);
+    expect(arrTest).not.toBe(arrRight);
+});
